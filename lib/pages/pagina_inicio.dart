7589ececec258b1c02406303ca6f181a64305e73
Camila Morales <scmorales1@espe.edu.ec>
2024-07-16 10:53:48 -0500

    Suma arreglado

--- lib/pages/pagina_inicio.dart
+++ lib/pages/pagina_inicio.dart
@@ -1,45 +1,47 @@
 import 'package:flutter/material.dart';
 
-class PaginaInicio extends StatefulWidget{
-  const PaginaInicio({Key? key}):super(key: key);
+class PaginaInicio extends StatefulWidget {
+  const PaginaInicio({Key? key}) : super(key: key);
   @override
-  State<PaginaInicio>createState()=>_Widget029State();
+  State<PaginaInicio> createState() => _Widget029State();
 }
 
-class _Widget029State extends State<PaginaInicio>{
+class _Widget029State extends State<PaginaInicio> {
   @override
-  Widget build (BuildContext context){
+  Widget build(BuildContext context) {
     return Center(
-      child: ElevatedButton(
-      child: const Text('Modal botton sheet'),
-      onPressed:(){
-        showModalBottomSheet(
-          context: context, 
-          builder: (BuildContext context){
-          return SizedBox(
-            height: 400,
-            child: Center(
-              child: ElevatedButton(
-                child: const Text('Close'),
-                onPressed: (){
-                  Navigator.pop(context);
-                },
-              ),
-            ),
-          );
-          });
-      },
-    ),
-
+      child: Column(
+        children: [
+          ElevatedButton(
+            child: const Text('Modal botton sheet'),
+            onPressed: () {
+              showModalBottomSheet(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return SizedBox(
+                      height: 400,
+                      child: Center(
+                        child: ElevatedButton(
+                          child: const Text('Close'),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                        ),
+                      ),
+                    );
+                  });
+            },
+          ),
+          PaginaSuma()
+        ],
+      ),
     );
   }
 }
 
-/*
-class PaginaInicio extends StatelessWidget {
+class PaginaSuma extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     // Controladores para los campos de texto
     final TextEditingController numero1Controller = TextEditingController();
     final TextEditingController numero2Controller = TextEditingController();
@@ -52,88 +54,81 @@ class PaginaInicio extends StatelessWidget {
       int num2 = int.tryParse(numero2Controller.text) ?? 0;
       // Calcular la suma
       int suma = num1 + num2;
-      // Actualizar el campo de texto del resultado 
+      // Actualizar el campo de texto del resultado
       resultadoController.text = suma.toString();
     }
 
-    return Scaffold(
-      body: SafeArea(
-        child: Container(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            children: [
-              // Título de la página
-              const Text(
-                'OPERACION DE SUMA',
-                style: TextStyle(fontSize: 30, color: Colors.black),
-              ),
+    return Column(
+      children: [
+        // Título de la página
+        const Text(
+          'OPERACION DE SUMA',
+          style: TextStyle(fontSize: 30, color: Colors.black),
+        ),
 
-              const SizedBox(height: 20), 
+        const SizedBox(height: 20),
 
-              // Campo de texto para ingresar el primer número
-              TextField(
-                controller: numero1Controller,
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  labelText: 'Ingrese número 1',
-                  labelStyle: TextStyle(fontSize: 15, color: Colors.grey.shade400),
-                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-                ),
-              ),
-
-              const SizedBox(height: 20), 
-
-              // Campo de texto para ingresar el segundo número
-              TextField(
-                controller: numero2Controller,
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  labelText: 'Ingrese número 2',
-                  labelStyle: TextStyle(fontSize: 15, color: Colors.grey.shade400),
-                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-                ),
-              ),
-
-              const SizedBox(height: 20), 
-
-              // Botón para realizar la suma
-              Container(
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.circular(5),
-                ),
-                child: TextButton(
-                  onPressed: () {
-                    // Llamar a la función sumar al presionar el botón
-                    sumar();
-                  },
-                  child: const Text(
-                    "Sumar",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ),
-
-              const SizedBox(height: 20), 
-
-              // Campo de texto para mostrar el resultado de la suma
-              TextField(
-                controller: resultadoController,
-                readOnly: true, // Para evitar la edición manual del resultado
-                decoration: InputDecoration(
-                  labelText: 'Resultado',
-                  labelStyle: TextStyle(fontSize: 15, color: Colors.grey.shade400),
-                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-                ),
-              ),
-            ],
+        // Campo de texto para ingresar el primer número
+        TextField(
+          controller: numero1Controller,
+          keyboardType: TextInputType.number,
+          decoration: InputDecoration(
+            labelText: 'Ingrese número 1',
+            labelStyle: TextStyle(fontSize: 15, color: Colors.grey.shade400),
+            border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
           ),
         ),
-      ),
+
+        const SizedBox(height: 20),
+
+        // Campo de texto para ingresar el segundo número
+        TextField(
+          controller: numero2Controller,
+          keyboardType: TextInputType.number,
+          decoration: InputDecoration(
+            labelText: 'Ingrese número 2',
+            labelStyle: TextStyle(fontSize: 15, color: Colors.grey.shade400),
+            border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+          ),
+        ),
+
+        const SizedBox(height: 20),
+
+        // Botón para realizar la suma
+        Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: Colors.blue,
+            borderRadius: BorderRadius.circular(5),
+          ),
+          child: TextButton(
+            onPressed: () {
+              // Llamar a la función sumar al presionar el botón
+              sumar();
+            },
+            child: const Text(
+              "Sumar",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+
+        const SizedBox(height: 20),
+
+        // Campo de texto para mostrar el resultado de la suma
+        TextField(
+          controller: resultadoController,
+          readOnly: true, // Para evitar la edición manual del resultado
+          decoration: InputDecoration(
+            labelText: 'Resultado',
+            labelStyle: TextStyle(fontSize: 15, color: Colors.grey.shade400),
+            border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+          ),
+        ),
+      ],
     );
   }
-}*/
+}
