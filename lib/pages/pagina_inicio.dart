b88c91c2ef1d56d09bf6d4d802ec25f3c2647e2b
M4t3B4rriga <msbarriga@espe.edu.ec>
2024-07-16 10:08:55 -0500

    Widget-pantalla

--- lib/pages/pagina_inicio.dart
+++ lib/pages/pagina_inicio.dart
@@ -1,5 +1,41 @@
 import 'package:flutter/material.dart';
 
+class PaginaInicio extends StatefulWidget{
+  const PaginaInicio({Key? key}):super(key: key);
+  @override
+  State<PaginaInicio>createState()=>_Widget029State();
+}
+
+class _Widget029State extends State<PaginaInicio>{
+  @override
+  Widget build (BuildContext context){
+    return Center(
+      child: ElevatedButton(
+      child: const Text('Modal botton sheet'),
+      onPressed:(){
+        showModalBottomSheet(
+          context: context, 
+          builder: (BuildContext context){
+          return SizedBox(
+            height: 400,
+            child: Center(
+              child: ElevatedButton(
+                child: const Text('Close'),
+                onPressed: (){
+                  Navigator.pop(context);
+                },
+              ),
+            ),
+          );
+          });
+      },
+    ),
+
+    );
+  }
+}
+
+/*
 class PaginaInicio extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -10,4 +46,4 @@ class PaginaInicio extends StatelessWidget {
       ),
     );
   }
-}
+}*/
